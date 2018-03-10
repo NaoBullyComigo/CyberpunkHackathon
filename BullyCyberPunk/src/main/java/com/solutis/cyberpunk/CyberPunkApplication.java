d5cd33e1a4f7b115247ac58bd07be49eb1dd4d7d
Pedro Augusto Dultra Neves Correia <padncorreia@tjba.jus.br>
2018-03-10 14:16:28 -0300

    projeto java

---
+++ BullyCyberPunk/src/main/java/com/solutis/cyberpunk/CyberPunkApplication.java
@@ -0,0 +1,12 @@
+package com.solutis.cyberpunk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberPunkApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBullyApplication.class, args);
+	}
+}
+
